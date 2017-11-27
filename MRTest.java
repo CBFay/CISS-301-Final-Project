0426285e33a19f6e0f2e6a9a92ced51620ffd390
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2017-11-27 10:02:18 -0500

    Skeleton for interaction between classes

--- MRTest.java
+++ MRTest.java
@@ -3,9 +3,9 @@ class MRTest {
 	public static void main(String[] args) {
 		MorseReader MR = new MorseReader();
 		
-		MR.listen(0);
-		MR.listen(1);
-		MR.listen(2);
-		MR.toEnglish();
+		MR.listen(0); 	// button "wait"
+		MR.listen(1); 	// button "short"
+		MR.listen(2); 	// button "long"
+		MR.toEnglish(); // button "done"
 	}
 }
